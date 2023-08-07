ba1523ac6b8824f5995b413a528f85be88139285
nckenn <nckenn.developer@gmail.com>
2023-08-07 17:00:40 +0800

    chore: added tailwindcss forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
